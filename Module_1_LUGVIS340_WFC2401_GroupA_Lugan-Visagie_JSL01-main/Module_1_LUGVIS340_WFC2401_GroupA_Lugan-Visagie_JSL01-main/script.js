1483ab18777c92b8b33e566d8405d4792e5b2f2d
Lugan Visagie <visagielugan42@gmail.com>
2024-03-13 10:00:58 +0200

    Created variables

--- Module_1_LUGVIS340_WFC2401_GroupA_Lugan-Visagie_JSL01-main/Module_1_LUGVIS340_WFC2401_GroupA_Lugan-Visagie_JSL01-main/script.js
+++ Module_1_LUGVIS340_WFC2401_GroupA_Lugan-Visagie_JSL01-main/Module_1_LUGVIS340_WFC2401_GroupA_Lugan-Visagie_JSL01-main/script.js
@@ -1,6 +1,11 @@
 function validateSyntax() {
+    // add a console.log to check if onclick eventlistner works correctly
+    console.log("button clicked");
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
+    let petPrefix = "pet_"
+    //combine regular expressions into a single patter
+    let validInputRegex = new RegExp(`^${petPrefix}[0-9]+[a-zA-Z]+$`);
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
