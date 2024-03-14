f6f0824bcb132c34d09efce62e3b6455943809a2
Lugan Visagie <visagielugan42@gmail.com>
2024-03-14 10:20:31 +0200

    Updated color changes

--- Module_1_LUGVIS340_WFC2401_GroupA_Lugan-Visagie_JSL01-main/Module_1_LUGVIS340_WFC2401_GroupA_Lugan-Visagie_JSL01-main/script.js
+++ Module_1_LUGVIS340_WFC2401_GroupA_Lugan-Visagie_JSL01-main/Module_1_LUGVIS340_WFC2401_GroupA_Lugan-Visagie_JSL01-main/script.js
@@ -15,9 +15,13 @@ function validateSyntax() {
       // Check if input matches the combined pattern
     if (validInputRegex.test(input)) {
         result = 'valid Syntax';
+        document.getElementById('result').style.color = '#008000';
+        document.getElementById('indicater').style.backgroundColor = '#008000';
         console.log(result);
     } else {
         result = 'invalid Syntax';
+        document.getElementById('result').style.color = '#ff4500';
+        document.getElementById('indicater').style.backgroundColor = '#ff4500';
         console.log(result);
     }
 
